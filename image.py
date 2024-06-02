25bffe1a5740a5370c0c298c5cfd24c31d6559b5
vloziak <150846733+vloziak@users.noreply.github.com>
2024-06-02 22:59:13 +0300

    added enlarge image

--- image.py
+++ image.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2 as cv
+from matplotlib import pyplot as plt
 
-img = cv.imread('C:\\Users\\Professional\\Downloads\\Гоша.jpg')
-new_width = int(img.shape[1] * 0.75)
-new_height = int(img.shape[0] * 0.75)
+img = cv.imread('C:\\Users\\Professional\\Downloads\\photo_5393407080783798215_y.jpg')
+
+new_width = int(img.shape[1] * 0.55)
+new_height = int(img.shape[0] * 0.55)
 resized_img = cv.resize(img, (new_width, new_height))
-cv.imshow('Gosha', resized_img)
+cv.imshow('Image', resized_img)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
@@ -13,7 +15,6 @@ rotated_image = cv.rotate(resized_img, cv.ROTATE_90_CLOCKWISE)
 cv.imshow('Rotated Image', rotated_image)
 cv.waitKey(0)
 cv.destroyAllWindows()
-
 
 def reflect_image(image, direction):
     if direction == 'y':
@@ -29,5 +30,16 @@ cv.destroyAllWindows()
 
 reflected_image_x = reflect_image(resized_img,'x')
 cv.imshow('Reflected image x', reflected_image_x)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+pts1 = np.float32([[76, 75], [468, 72], [48, 487], [489, 490]])
+pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])
+
+M = cv.getPerspectiveTransform(pts1, pts2)
+
+dst = cv.warpPerspective(img, M, (300, 300))
+
+cv.imshow('Enlarged image', dst)
 cv.waitKey(0)
 cv.destroyAllWindows()
